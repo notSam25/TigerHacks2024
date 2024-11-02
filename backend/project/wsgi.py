9816acfae6f89cbe03de850b576229e645b1b57d
Sayam Gupta <gupta.sayam2006@gmail.com>
2024-11-02 02:31:39 -0500

    changed build scripots

--- backend/project/wsgi.py
+++ backend/project/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
-application = get_wsgi_application()
+try:
+    application = get_wsgi_application()
+except Exception as e:
+    print(f"Error loading WSGI application: {e}")
+    raise
