18aaeef297926a3e3d2d96948856dce3a4dbb450
Sayam Gupta <gupta.sayam2006@gmail.com>
2024-11-03 02:44:17 -0600

    setup backend part of auth

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -1,6 +1,13 @@
 # project urls.py
 from django.urls import path, include
+from rest_framework_simplejwt import views as jwt_views
+from django.contrib import admin
+
 
 urlpatterns = [
-    path('', include('nutrition_api.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("nutrition_api.urls")),
+    path("token/", jwt_views.TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", jwt_views.TokenRefreshView.as_view(), name="token_refresh"),
+    path("", include("jwt_authentication.urls")),
 ]
